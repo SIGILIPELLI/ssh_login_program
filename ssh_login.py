3d5acf222f630542cfc056110e5bce982c028d68
SIGILIPELLI <48328572+SIGILIPELLI@users.noreply.github.com>
2019-05-06 23:12:19 +0530

    Update ssh_login.py

--- ssh_login.py
+++ ssh_login.py
@@ -8,13 +8,13 @@ try:
     s.login (hostname, username, password)
     s.sendline ('uptime')   # run a command
     s.prompt()             # match the prompt
-    print s.before          # print everything before the prompt.
+    print(s.before)          # print everything before the prompt.
     s.sendline ('ls -l')
     s.prompt()
-    print s.before
+    print(s.before)
     s.sendline ('df')
     s.prompt()
-    print s.before
+    print(s.before)
     s.logout()
 except pxssh.ExceptionPxssh, e:
     print "pxssh failed on login."
