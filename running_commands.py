0d14ac29cb01fcb468bf19bebf19542ee8db0a86
SIGILIPELLI <48328572+SIGILIPELLI@users.noreply.github.com>
2019-05-06 23:09:54 +0530

    running_commands_using_ssh
    here user will login and check CPU up time using system commands.

---
+++ running_commands.py
@@ -0,0 +1,14 @@
+import pxssh
+s = pxssh.pxssh()
+if not s.login ('localhost', 'myusername', 'mypassword'):
+    print "SSH session failed on login."
+    print str(s)
+else:
+    print "SSH session login successful"
+    s.sendline ('uptime')
+    s.prompt()         # match the prompt
+    print s.before     # print everything before the prompt.
+    s.logout()
+    
+#We can also execute multiple command like this:
+s.sendline ('uptime;df -h')
