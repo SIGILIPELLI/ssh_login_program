b1cc8c765f975c8e3c3b237deff0149c659e1d09
SIGILIPELLI <48328572+SIGILIPELLI@users.noreply.github.com>
2019-05-07 10:47:51 +0530

    Update telnet_login.py

--- telnet_login.py
+++ telnet_login.py
@@ -1,13 +1,40 @@
-import os,re,telnetlib
-host = "10.xxx.xxx.xxx"
-port = 23
+import sys
+import telnetlib
+import time
 
-telnet = telnetlib.Telnet()
-telnet.open(host, port)
-telnet.write('loginID\r\n')
-telnet.write('Password\r\n')
-out = telnet.read_until("DB>", 5)
-telnet.write('show cable modem reg\r\n') #Mycommand
-out = telnet.read_until("DB>", 5)
-telnet.write('quit\r\n')
-telnet.close()
+password = "cisco123"
+command = "show interface status"
+
+##with open ("list.txt", "r") as devicelist:
+##    hostlist = []
+##    hostlist=devicelist.readlines()
+##    print(hostlist)
+hostlist= [ ("10.197.81.80","",""),]
+
+for host in hostlist:
+
+        cmd1 = "enable"
+        tn = telnetlib.Telnet(host[0])
+
+        time.sleep(2)
+        tn.read_until(b"Password: ")
+        tn.write(password.encode('ascii') + b"\n")
+        time.sleep(2)
+        tn.write(cmd1.encode('ascii') + b"\n")
+        time.sleep(2)
+        tn.write(password.encode('ascii') + b"\n")
+        time.sleep(2)
+        tn.write(command.encode('ascii') + b"\n")
+        time.sleep(2)
+        tn.write(b"\n")
+        time.sleep(2)
+        tn.write(b"\n")
+        time.sleep(2)
+        tn.write(b"exit\n")
+        lastpost = tn.read_all().decode('ascii')
+        op=open ("output.txt", "w")
+        op.write(lastpost)
+        print("writing to file")
+        op.close()
+        print(lastpost)
+        tn.close()
